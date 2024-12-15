8b655a69430b05231e2486ddc61d32910709d1ab
rgabriel01 <raymond.m.gabriel@gmail.com>
2024-12-15 21:39:51 +0800

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  use_doorkeeper
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  get "team_members" => "team_members#index"
+  get "villains" => "villains#index"
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
